febfe76a72761692d1d0fb07452f5fc9c8d43b6a
Dallas <dallaskelleydice@gmail.com>
2019-09-23 23:56:25 -0500

    Updates

---
+++ precache-manifest.885c4a7175967633344b78bd8a34866e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f929dd417b0492f5abc1aae33efc5645",
+    "url": "/vivaldiclone/index.html"
+  },
+  {
+    "revision": "4d184e78a63eeeb22f8e",
+    "url": "/vivaldiclone/static/css/main.12d7b3c6.chunk.css"
+  },
+  {
+    "revision": "a062739dfd191743ef43",
+    "url": "/vivaldiclone/static/js/2.3c48b98e.chunk.js"
+  },
+  {
+    "revision": "4d184e78a63eeeb22f8e",
+    "url": "/vivaldiclone/static/js/main.3d2fc47b.chunk.js"
+  },
+  {
+    "revision": "7433364f5e2ca7c14e0d",
+    "url": "/vivaldiclone/static/js/runtime~main.10cdbc9d.js"
+  }
+]);
